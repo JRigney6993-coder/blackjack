1d84257604d9da5ad7f0bb943fb4974425e4707e
JRigney6993-coder <77342747+JRigney6993-coder@users.noreply.github.com>
2023-10-11 14:55:02 -0700

    people

---
+++ models/people.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const User = new mongoose.Schema({
+    Username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    Email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    Password: {
+        type: String,
+        required: true,
+    },
+});
+
+
+module.exports = mongoose.model('Users', User);
+
+
