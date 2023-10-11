f82a408c040a7cf707f2425ab6767d27bcb4d9ad
JRigney6993-coder <77342747+JRigney6993-coder@users.noreply.github.com>
2023-10-11 13:56:46 -0700

    save

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+const routes = require('./routes/router');
+
+// Load env variables
+dotenv.config({ path: './.env' });
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+connectDB();
+app.use(routes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+});
