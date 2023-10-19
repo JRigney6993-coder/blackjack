8e7fb14dc788cb611ff02f6249eedd4eee7aec43
ContriverJake <77342747+JRigney6993-coder@users.noreply.github.com>
2023-10-18 23:58:19 -0700

    save

--- routes/game.js
+++ routes/game.js
@@ -1,56 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const deckData = require('../data/deck');
+
+// Load deck and user model
+const deck = require('../data/deck.js');
+let workingDeck = [...deck];
 const User = require('../models/user');
 
-let deck = [];
-const setupDeck = () => [...deckData];
+// Simple in-memory storage of dealer and player hands
+let dealerCards = [];
+let playerCards = [];
 
 router.get('/play', (req, res) => {
     res.render('pages/table', {
         user: req.user
     });
 });
-
-router.get('/setupDeck', (req, res) => {
-    resetDeck(deck);
-    res.send({ message: "Deck setup completed", deck: deck });
-});
-
-router.get('/deal', (req, res) => {
-    const { player, dealer } = deal(deck);
-    res.send({ player, dealer });
-});
-
-router.post('/hit', (req, res) => {
-    if (deck.length === 0) {
-        resetDeck(deck);
-    }
-    const newCard = getRandomCard(deck);
-    res.send(newCard);
-});
-
-router.post('/determineWinner', async (req, res) => {
-    let { playerHand, dealerHand } = req.body;
-
-    // Let the dealer play their turn fully
-    dealerHand = dealerPlay(dealerHand, deck);
-
-    const winner = determineWinner(playerHand, dealerHand);
-
-    if (req.isAuthenticated()) {
-        const userId = req.user._id;
-
-        if (winner === "Player") {
-            await User.findByIdAndUpdate(userId, { $inc: { wins: 1 } });
-        } else if (winner === "Dealer") {
-            await User.findByIdAndUpdate(userId, { $inc: { losses: 1 } });
-        }
-    }
-
-    res.send({ winner });
-});
-
 
 router.get('/topPlayers', async (req, res) => {
     try {
@@ -71,7 +35,6 @@ router.get('/topPlayers', async (req, res) => {
                 $limit: 10
             }
         ]);
-
         res.send(topPlayers);
     } catch (error) {
         res.status(500).send({ message: "Error fetching top players", error });
@@ -79,58 +42,99 @@ router.get('/topPlayers', async (req, res) => {
 });
 
 
-// Blackjack game functions
-function resetDeck(deck) {
-    const newDeck = setupDeck();
-    deck.length = 0;
-    deck.push(...newDeck);
-}
+router.get('/start', (req, res) => {
+    dealerCards = [];
+    playerCards = [];
+    
+    dealerCards.push(drawCard(deck), drawCard(deck));
+    playerCards.push(drawCard(deck), drawCard(deck));
+    
+    res.json({
+        dealerCards: dealerCards,
+        playerCards: playerCards
+    });
+});
 
-function getRandomCard(deck) {
-    const randomIndex = Math.floor(Math.random() * deck.length);
-    return deck.splice(randomIndex, 1)[0];
-}
-
-function deal(deck) {
-    return {
-        player: [getRandomCard(deck), getRandomCard(deck)],
-        dealer: [getRandomCard(deck), getRandomCard(deck)]
-    };
-}
-
-function hit(hand, deck) {
-    hand.push(getRandomCard(deck));
-    return hand;
-}
-
-function calculateTotal(hand) {
-    if (!Array.isArray(hand)) {
-        console.error("hand is not an array:", hand);
-        return 0;
+router.get('/hit', async (req, res) => {
+    if (playerCards.length < 5) {
+        playerCards.push(drawCard(deck));
     }
 
-    const cardValues = {
-        'j': 10,
-        'q': 10,
-        'k': 10,
-        'a': 11
-    };
+    if (calculatePoints(playerCards) > 21) {
+        await updatePlayerRecord(req.user._id, 'loss');
+        return res.json({
+            playerCards: playerCards,
+            message: 'Player busts! Dealer wins!'
+        });
+    } else if (playerCards.length === 5) {
+        return res.json({
+            playerCards: playerCards,
+            message: 'Player automatically wins with 5 cards!'
+        });
+    }
 
+    return res.json({ playerCards: playerCards });
+
+});
+
+router.get('/stand', async (req, res) => {
+    while (calculatePoints(dealerCards) <= 16) {
+        dealerCards.push(drawCard(deck));
+    }
+
+    const playerPoints = calculatePoints(playerCards);
+    const dealerPoints = calculatePoints(dealerCards);
+    let result = 'draw';
+    let message = "It's a draw!";
+
+    if (playerPoints > 21 || (dealerPoints <= 21 && dealerPoints > playerPoints)) {
+        result = 'dealer';
+        message = 'Dealer wins!';
+        await updatePlayerRecord(req.user._id, 'loss');
+    } else if (dealerPoints > 21 || playerPoints > dealerPoints) {
+        result = 'player';
+        message = 'Player wins!';
+        await updatePlayerRecord(req.user._id, 'win');
+    }
+
+    res.json({
+        dealerCards: dealerCards,
+        result: result,
+        message: message
+    });
+});
+
+router.get('/newGame', (req, res) => {
+    const gameData = newGame();
+    res.json(gameData);
+});
+
+// Game logic stuff yay!
+
+function drawCard() {
+    if (workingDeck.length === 0) {
+        workingDeck = [...deck];
+    }
+    return workingDeck.splice(Math.floor(Math.random() * workingDeck.length), 1)[0];
+}
+
+
+function calculatePoints(cards) {
     let total = 0;
-    let aces = 0;
+    let aces = 0; 
 
-    for (let card of hand) {
-        if (card.value === 'a') {
-            total += cardValues[card.value];
+    for (const card of cards) {
+        if (Array.isArray(card.value)) {  
+            total += 11;
             aces++;
-        } else if (["j", "q", "k"].includes(card.value)) {
-            total += cardValues[card.value];
+        } else if (typeof card.value === 'string') {  
+            total += 10;
         } else {
-            total += card.value;
+            total += card.value; 
         }
     }
 
-    // If total is over 21 and we have Aces, reduce total by 10 for each Ace
+    // Convert aces from 11 to 1 if total is over 21
     while (total > 21 && aces > 0) {
         total -= 10;
         aces--;
@@ -139,26 +143,33 @@ function calculateTotal(hand) {
     return total;
 }
 
-function dealerPlay(dealerHand, deck) {
-    while (calculateTotal(dealerHand) < 17) {
-        dealerHand.push(getRandomCard(deck));
+async function updatePlayerRecord(userId, outcome) {
+    try {
+        if (outcome === 'win') {
+            await User.findByIdAndUpdate(userId, { $inc: { wins: 1 } });
+        } else if (outcome === 'loss') {
+            await User.findByIdAndUpdate(userId, { $inc: { losses: 1 } });
+        }
+    } catch (error) {
+        console.error("Error updating player record:", error);
     }
-    return dealerHand;
 }
 
 
-function determineWinner(playerHand, dealerHand) {
-    const playerTotal = calculateTotal(playerHand);
-    const dealerTotal = calculateTotal(dealerHand);
+function newGame() {
+    dealerCards = [];
+    playerCards = [];
     
-
-    if (playerTotal > 21) return "Dealer"; // Player busts
-    if (dealerTotal > 21) return "Player"; // Dealer busts
-
-    // Draw should be the last condition checked
-    if (playerTotal === dealerTotal) return "Draw"; 
-
-    return playerTotal > dealerTotal ? "Player" : "Dealer";
+    dealerCards.push(drawCard(deck), drawCard(deck));
+    playerCards.push(drawCard(deck), drawCard(deck));
+    
+    return {
+        dealerCards: [{
+            ...dealerCards[0],
+            hidden: true
+        }, dealerCards[1]],
+        playerCards: playerCards
+    };
 }
 
 
